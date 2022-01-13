97f5d454b35af96a8f4bdbb4245cc29eb6cd6dfd
Hien Tran <79175744+hientran-sudo@users.noreply.github.com>
2022-01-13 13:40:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const myProfile = new Profile(
     2022,
     "Software Engineering",
     false,
-    "2021-08-08.jpg"
-
+    "2021-08-08.jpg",
+    "December 27, 2021 8:00:00 PST"
 );
 const main = document.querySelector(".maincontent");
 const content = `
@@ -20,16 +20,17 @@ const content = `
        <li class="profile__school">School:<span> ${myProfile.school.sn}</span></li>
        <li class="profile__school">Major:<span> ${myProfile.school.m}</span></li>
        <li class="profile__school">Class year:<span> ${myProfile.school.cy}</span></li>
-       <li class="profile__school">Graduate:<span> ${myProfile.graduate}</span></li>      
+       <li class="profile__school">Graduate:<span> ${myProfile.graduate}</span></li>
+       <li class="profile__school">Age of the site:<span> ${myProfile.siteAge()}</span></li>      
      </ul>
 `;
-
 const navContent =`
 <li><a href="#">Home</a></li>
 <li><a href="#">About</a></li>
 <li><a href="#">Projects</a></li>
 <li><a href="#">Contact</a></li>
 `;
+
 const newArticle  = document.createElement("article")
 newArticle.classList.add("profile")
 newArticle.setAttribute("id","my")
@@ -59,7 +60,10 @@ document.querySelector(".right").style.backgroundColor=color;
 document.querySelector(".right .color-value").innerHTML=color;
 
 headingColor();
-console.log(myProfile.graduate? "graduated":"in progress");
+
+
+
+
 
 
 
