c97db14786985b12bbc8016a0344093625f3b900
Hien Tran <79175744+hientran-sudo@users.noreply.github.com>
2022-01-18 16:25:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,8 @@ const myProfile = new Profile(
     "Fairfield University",
     2022,
     "Software Engineering",
-    false,
-    "2021-08-08.jpg",
-    "December 27, 2021 8:00:00 PST"
+    "2021-08-08.jpg"
+
 );
 const main = document.querySelector(".maincontent");
 const content = `
@@ -20,17 +19,16 @@ const content = `
        <li class="profile__school">School:<span> ${myProfile.school.sn}</span></li>
        <li class="profile__school">Major:<span> ${myProfile.school.m}</span></li>
        <li class="profile__school">Class year:<span> ${myProfile.school.cy}</span></li>
-       <li class="profile__school">Graduate:<span> ${myProfile.graduate}</span></li>
-       <li class="profile__school">Age of the site:<span> ${myProfile.siteAge()}</span></li>      
+       
      </ul>
 `;
+
 const navContent =`
 <li><a href="#">Home</a></li>
 <li><a href="#">About</a></li>
 <li><a href="#">Projects</a></li>
 <li><a href="#">Contact</a></li>
 `;
-
 const newArticle  = document.createElement("article")
 newArticle.classList.add("profile")
 newArticle.setAttribute("id","my")
@@ -61,12 +59,49 @@ document.querySelector(".right .color-value").innerHTML=color;
 
 headingColor();
 
+const Experience = {
+  desc:"list of my Experience",
+  javascript:2,
+  java:3
+}
 
+const addEx= (current)=>{
+  const newArticle = document.createElement("article");
+  newArticle.innerHTML=`
+  <h1>${current.desc}</h1>
+  <ul>
+  <li>Javascript: ${current.javascript}</li>
+  <li>Javas: ${current.java}</li>
+  </ul>
+  `;
+  return newArticle;
+}
+const testmain = document.querySelector("testmain");
+testmain.append(addEx(Experience));
 
+const container = document.querySelector(".container");
+const button = document.querySelector(".cta-button");
+const posX = document.querySelector(".posX span");
+const posY = document.querySelector(".posY span");
 
+button.addEventListener("click",()=>{
+  button.classList.toggle("active");
+  console.log("button was clicked! ");
+}, false)
 
+const mousePosition = (event)=>{
+  posX.innerText = event.pageX;
+  posY.innerText = event.pageY;
+};
 
+window.addEventListener("mousemove",mousePosition,false);
 
+container.addEventListener("mouseenter",()=>{
+  container.classList.add("blue");
+}, false
+);
 
-
-
+container.addEventListener("mouseleave",()=>{
+  container.classList.remove("blue");
+}, false
+);
